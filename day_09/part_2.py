ace9e2afa5a895fd7177d4aed29ddecba084356a
Peter Scriven <peter.scriven@trademe.co.nz>
2022-12-11 13:54:51 +1300

    day 9

--- day_09/part_2.py
+++ day_09/part_2.py
@@ -8,81 +8,110 @@ class Action():
         self.amount = int(parts[1])
 
 
-    def move(self, rope, t_history) -> str:
-
+    def move(self, rope, t_history):
         for _ in range(self.amount):
+            tail_pos = rope.move(self.direction)
+            p_tuple = (tail_pos.x, tail_pos.y)
+            if not p_tuple in t_history:
+                t_history.append(p_tuple)
 
-            # move up
-            if self.direction == 'U':
-                for i in range(len(rope) - 1):
-                    c_head = rope[i]
-                    c_tail = rope[i+1]
-
-                    if (c_head.y > c_tail.y):
-                        c_tail.x = c_head.x
-                        c_tail.y = c_head.y
-                    c_head.y = c_head.y + 1
-                    rope[i] = c_head
-                    rope[i+1] = c_tail
-                t_history.append((c_tail.x, c_tail.y))
-
-
-            # move right
-            if self.direction == 'R':
-                for i in range(len(rope) - 1):
-                    c_head = rope[i]
-                    c_tail = rope[i+1]
-
-                    if (c_head.x > c_tail.x):
-                        c_tail.x = c_head.x
-                        c_tail.y = c_head.y
-                    c_head.x = c_head.x + 1
-                    rope[i] = c_head
-                    rope[i+1] = c_tail
-                t_history.append((c_tail.x, c_tail.y))
-
-
-            # move left
-            if self.direction == 'L':
-                for i in range(len(rope) - 1):
-                    c_head = rope[i]
-                    c_tail = rope[i+1]
-
-                    if (c_head.x < c_tail.x):
-                        c_tail.x = c_head.x
-                        c_tail.y = c_head.y
-                    c_head.x = c_head.x - 1
-                    rope[i] = c_head
-                    rope[i+1] = c_tail
-                t_history.append((c_tail.x, c_tail.y))
-
-            # move down
-            if self.direction == 'D':
-                for i in range(len(rope) - 1):
-                    c_head = rope[i]
-                    c_tail = rope[i+1]
-
-                    if (c_head.y < c_tail.y):
-                        c_tail.x = c_head.x
-                        c_tail.y = c_head.y
-                    c_head.y = c_head.y - 1
-                    rope[i] = c_head
-                    rope[i+1] = c_tail
-                t_history.append((c_tail.x, c_tail.y))
-
-            return (rope, t_history)
-
+        return (rope, t_history)
 
 
 class Knot():
     name = ''
     x = 0
     y = 0
-
     def __init__(self, name):
-        self.name = name
+        self.name = 'H' if name == 0 else name
         self.x = 0
         self.y = 0
+
+
+class Rope():
+    segments = []
+
+    def __init__(self, length):
+        self.segments = [Knot(i) for i in range(length)]
+
+
+    def move(self, direction):
+        if direction == 'U':
+            self.move_segment(0, 0, 1)
+        if direction == 'R':
+            self.move_segment(0, 1, 0)
+        if direction == 'L':
+            self.move_segment(0, -1, 0)
+        if direction == 'D':
+            self.move_segment(0, 0, -1)
+        return self.segments[-1]
+
+
+    def __repr__(self):
+        x = [k.x for k in self.segments]
+        y = [k.y for k in self.segments]
+
+        min_x = min(min(x), 0)
+        max_x = max(x)
+        min_y = min(min(y), 0)
+        max_y = max(y)
+
+        grid = []
+        for _ in range(min_y, max_y+1):
+            row = []
+            for _ in range(min_x, max_x+1):
+                row.append('.')
+            grid.append(row)
+
+        for s in self.segments:
+            grid[s.y - min_y][s.x - min_x] = f'{s.name}'
+
+        out = ''
+        for line in grid:
+            l = ''.join(line)
+            out = f'{l}\n{out}'
+
+        return out
+
+
+    def move_segment(self, index, dx, dy):
+        head = self.segments[index]
+
+        if index < len(self.segments) - 1:
+            tail = self.segments[index+1]
+
+            p_dx = head.x - tail.x
+            p_dy = head.y - tail.y
+
+            dx_t = 0
+            dy_t = 0
+
+            # Same position
+            if p_dx == 0 and p_dy == 0:
+                pass
+
+            # Diagonal position
+            elif abs(p_dx) + abs(p_dy) > 1:
+                if p_dx == dx or (dx == 0 and p_dy == dy):
+                    dx_t = p_dx
+
+                if p_dy == dy or (dy == 0 and p_dx == dx):
+                    dy_t = p_dy
+
+            # Normal position
+            else:
+                if p_dx == dx or (p_dx == 0 and p_dy == dy):
+                    dx_t = dx
+
+                if p_dy == dy or (p_dy == 0 and p_dx == dx):
+                    dy_t = dy
+
+            if dx_t != 0 or dy_t != 0:
+                self.move_segment(index+1, dx_t, dy_t)
+
+        head.x = head.x + dx
+        head.y = head.y + dy
+
 
 
 
@@ -95,21 +124,17 @@ for line in lines:
     actions.append(Action(line))
 
 
+rope = Rope(10)
 
-
-# 284 - too low!!!!
-
-
-rope = [Knot(i) for i in range(10)]
 
 t_history = []
-t_history.append((rope[-1].x, rope[-1].y))
+t_history.append((0, 0))
 
 
 for a in actions:
     rope, t_history = a.move(rope, t_history)
-
-
+    print(a.direction, a.amount)
+    print(rope)
 
 
 print(len(set(t_history)))
